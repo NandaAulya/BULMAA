911e4f308b0373eb3544ba65705d9d56f70f049c
NandaAulya <nandhakampir@gmail.com>
2024-10-11 12:42:16 +0700

    [+]

--- js/text.js
+++ js/text.js
@@ -7,19 +7,17 @@ function typeText(element, text, delay) {
             index++;
             setTimeout(type, delay);
         } else {
-            // Setelah selesai, hapus teks dan mulai lagi
             setTimeout(() => {
-                element.textContent = ''; // Kosongkan teks
-                index = 0; // Reset index
-                type(); // Mulai lagi
-            }, 400); // Waktu tunggu sebelum mulai lagi (dalam ms)
+                element.textContent = ''; 
+                index = 0; 
+                type(); 
+            }, 400); 
         }
     }
 
     type();
 }
 
-// Memanggil fungsi dengan teks yang diinginkan
 const textToType = "Welcome to my portofolio Website👋🏻";
 const typingElement = document.getElementById("typing-effect");
-typeText(typingElement, textToType, 100); // 100 ms delay
+typeText(typingElement, textToType, 100);
