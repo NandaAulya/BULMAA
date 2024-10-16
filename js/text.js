bfd07b08c05d92723f6aa918c83d5b614158a543
NandaAulya <nandhakampir@gmail.com>
2024-10-16 08:53:05 +0700

    [+}

--- js/text.js
+++ js/text.js
@@ -2,10 +2,17 @@ function typeText(element, text, delay) {
     let index = 0;
 
     function type() {
-        if (index < text.length) {
+        // Jika index lebih kecil dari panjang teks dikurangi 2, cetak satu per satu karakter
+        if (index < text.length - 4) {
             element.textContent += text.charAt(index);
             index++;
             setTimeout(type, delay);
+        // Tampilkan dua karakter terakhir sekaligus
+        } else if (index == text.length - 4) {
+            element.textContent += text.slice(index, index + 4);
+            index += 4;
+            setTimeout(type, delay);
+        // Setelah semua karakter tampil, reset teks untuk memulai ulang
         } else {
             setTimeout(() => {
                 element.textContent = ''; 
